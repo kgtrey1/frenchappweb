ba01c9bcfde2e2e29339402af62e3aab1e66c05e
Kevin Gouyet <kevin.gouyet@frenchappweb.com>
2022-07-26 12:00:12 +0200

    add: basic auth flow

--- src/Containers/Login/Login.tsx
+++ src/Containers/Login/Login.tsx
@@ -1,13 +1,63 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, Pressable, StyleSheet } from 'react-native'
+
+import { useLoginMutation } from '@/Services/Auth'
+import { useNavigation } from '@react-navigation/native'
 
 const Login: React.FC<{}> = (): JSX.Element => {
+    const { navigate } = useNavigation()
+
+    const [login, { error, isSuccess }] = useLoginMutation()
+    const [mail, setMail] = React.useState<string>('')
+    const [password, setPassword] = React.useState<string>('')
+
+    React.useEffect(() => {
+        if (isSuccess) {
+            navigate('HomeScreen')
+        }
+    }, [isSuccess])
+
     return (
-        <View>
-            <Text>Login screen</Text>
+        <View style={style.container}>
+            <Text>{JSON.stringify(error)}</Text>
+            <TextInput
+                placeholder="Username"
+                onChangeText={setMail}
+                style={style.input}
+            />
+            <TextInput
+                autoCorrect={false}
+                autoCapitalize={'none'}
+                placeholder="Password"
+                onChangeText={setPassword}
+                style={style.input}
+            />
+            <Pressable
+                style={style.button}
+                onPress={() => login({ username: mail, password })}
+            >
+                <Text>Login</Text>
+            </Pressable>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        width: '80%',
+        height: 40,
+        marginVertical: 10,
+    },
+    button: {
+        backgroundColor: 'cyan',
+    },
+})
 
 Login.defaultProps = {}
 
--- src/Containers/index.tsx
+++ src/Containers/index.tsx
@@ -1,1 +1,2 @@
 export { default as Login } from '@/Containers/Login/Login'
+export { default as Home } from '@/Containers/Home/Home'
